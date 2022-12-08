0bc2d7bdcc675ec0620f3cab9fbb44623649c354
Dementan <masav7986@gmail.com>
2022-12-08 20:53:27 +0300

    write a program to calculate the value of the discriminant

---
+++ main.py
@@ -0,0 +1,10 @@
+# write a program to calculate the value of the discriminant
+# D = b**2 - 4*a*c discriminant formula
+
+x = float(input("Enter value x: "))
+y = float(input("Enter value y: "))
+z = float(input("Enter value z: "))
+# d = discriminant
+d = (y**2)-(4*x*z)
+print(d)
+
